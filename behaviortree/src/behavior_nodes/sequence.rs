02ce4209402bb7c686470d1419a221599caef5d8
Niket Naidu <coder137@gmail.com>
2023-12-03 16:41:11 -0800

    Update sequence.rs
    Unit tests with mocking for various scenarios

--- behaviortree/src/behavior_nodes/sequence.rs
+++ behaviortree/src/behavior_nodes/sequence.rs
@@ -25,8 +25,8 @@ where
             }
         }
 
-        let next_status = self.current_action.tick(dt, shared);
-        let new_status = match next_status {
+        let child_status = self.current_action.tick(dt, shared);
+        let new_status = match child_status {
             Status::Success => {
                 let next_index = self.index + 1;
                 match self.behaviors.get(next_index) {
@@ -46,8 +46,8 @@ where
             }
             _ => {
                 // Failure | Running
-                self.current_action_status = Some(next_status);
-                next_status
+                self.current_action_status = Some(child_status);
+                child_status
             }
         };
         self.status = Some(new_status);
@@ -60,7 +60,6 @@ where
                 self.current_action.halt();
             }
         }
-        self.status = None;
     }
 }
 
@@ -80,4 +79,59 @@ where
             current_action_status: None,
         }
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        test_behavior_interface::{TestActions, TestShared},
+        Action, Behavior, Status,
+    };
+
+    #[test]
+    fn test_sequence_success() {
+        let mut sequence = SequenceState::new(vec![Behavior::Action(TestActions::Success)]);
+        assert_eq!(sequence.status, None);
+
+        let mut shared = TestShared::default();
+        let status = sequence.tick(0.1, &mut shared);
+        assert_eq!(status, Status::Success);
+
+        let status = sequence.tick(0.1, &mut shared);
+        assert_eq!(status, Status::Success);
+    }
+
+    #[test]
+    fn test_sequence_failure() {
+        let mut sequence = SequenceState::new(vec![Behavior::Action(TestActions::Failure)]);
+        assert_eq!(sequence.status, None);
+
+        let mut shared = TestShared::default();
+        let status = sequence.tick(0.1, &mut shared);
+        assert_eq!(status, Status::Failure);
+
+        let status = sequence.tick(0.1, &mut shared);
+        assert_eq!(status, Status::Failure);
+    }
+
+    #[test]
+    fn test_sequence_run_then_status() {
+        let mut sequence =
+            SequenceState::new(vec![Behavior::Action(TestActions::Run(2, Status::Failure))]);
+        assert_eq!(sequence.status, None);
+
+        let mut shared = TestShared::default();
+        let status = sequence.tick(0.1, &mut shared);
+        assert_eq!(status, Status::Running);
+
+        let status = sequence.tick(0.1, &mut shared);
+        assert_eq!(status, Status::Running);
+
+        let status = sequence.tick(0.1, &mut shared);
+        assert_eq!(status, Status::Failure);
+    }
+
+    #[test]
+    fn test_sequence_run_then_halt() {}
 }
