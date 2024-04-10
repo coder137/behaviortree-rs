134c31d17ea07751a4c990e1f35d234858364bc1
Niket Naidu <coder137@gmail.com>
2024-04-09 21:19:34 -0700

    Updated behavior interface with reset
    - Removed halt

--- behaviortree/src/behavior_interface.rs
+++ behaviortree/src/behavior_interface.rs
@@ -1,5 +1,5 @@
 use crate::{
-    behavior_nodes::{InvertState, SelectState, SequenceState, WaitState},
+    behavior_nodes::{SequenceState, WaitState},
     Behavior, State, Status,
 };
 
@@ -20,12 +20,21 @@ pub trait Action<S> {
     /// NOTE: See `BehaviorTree` implementation. User is not expected to invoke this manually
     fn tick(&mut self, dt: f64, shared: &mut S) -> Status;
 
+    /// Resets the current action
+    ///
+    /// Functionally equal to calling `Self::new(..)`
+    fn reset(&mut self);
+
     /// Halts the current action and `status` will be reported as None
     ///
     /// Function is only invoked when action is in `Status::Running` state
     ///
     /// Ticking after halting == `resume` operation
+    /// TODO, Do we need this?
+    /// * It might be good to have in the case of halt and resume operations
     fn halt(&mut self) {}
+
+    // TODO, on_start, on_end, on_halt, on_reset
 
     /// Decorator and Control type nodes need to know the state of its child(ren)
     /// User defined Action nodes do not need to override this function
@@ -48,8 +57,10 @@ where
             Behavior::Action(action) => action.to_action(),
             Behavior::Wait(target) => Box::new(WaitState::new(target)),
             Behavior::Sequence(behaviors) => Box::new(SequenceState::new(behaviors)),
-            Behavior::Select(behaviors) => Box::new(SelectState::new(behaviors)),
-            Behavior::Invert(behavior) => Box::new(InvertState::new(*behavior)),
+            Behavior::Select(_) => todo!(),
+            Behavior::Invert(_) => todo!(),
+            // Behavior::Select(behaviors) => Box::new(SelectState::new(behaviors)),
+            // Behavior::Invert(behavior) => Box::new(InvertState::new(*behavior)),
         }
     }
 }
@@ -64,45 +75,79 @@ pub mod test_behavior_interface {
     #[derive(Clone)]
     pub enum TestActions {
         /// Action returns success immediately
-        Success,
+        SuccessTimes { ticks: usize },
+        SuccessWithCb {
+            ticks: usize,
+            cb: fn(MockAction<TestShared>) -> MockAction<TestShared>,
+        },
         /// Action returns failure immediately
-        Failure,
-        /// Action runs for `usize` ticks and returns `status` in the next tick
+        FailureTimes { ticks: usize },
+        FailureWithCb {
+            ticks: usize,
+            cb: fn(MockAction<TestShared>) -> MockAction<TestShared>,
+        },
+        /// Action runs for `usize` ticks as Status::Running and returns `status` in the next tick
         ///
         /// Runs for a total of `usize + 1` ticks
-        Run(usize, Status),
+        Run { times: usize, output: Status },
+        RunWithCb {
+            times: usize,
+            output: Status,
+            cb: fn(MockAction<TestShared>) -> MockAction<TestShared>,
+        },
         /// Provides a user defined callback to simulate more complex scenarios
         Simulate(fn(MockAction<TestShared>) -> MockAction<TestShared>),
     }
 
     impl ToAction<TestShared> for TestActions {
         fn to_action(self) -> Box<dyn Action<TestShared>> {
-            let mut mock = MockAction::new();
             match self {
-                TestActions::Success => {
-                    mock.expect_tick()
-                        .once()
-                        .returning(|_dt, _shared| Status::Success);
-                    mock.expect_state().returning(|| State::NoChild);
+                TestActions::SuccessTimes { ticks } => {
+                    TestActions::SuccessWithCb { ticks, cb: |m| m }.to_action()
                 }
-                TestActions::Failure => {
+                TestActions::SuccessWithCb { ticks, cb } => {
+                    let mut mock = MockAction::new();
                     mock.expect_tick()
-                        .once()
+                        .times(ticks)
+                        .returning(|_, _| Status::Success);
+                    mock.expect_state().returning(|| State::NoChild);
+                    mock = cb(mock);
+                    Box::new(mock)
+                }
+                TestActions::FailureTimes { ticks } => {
+                    TestActions::FailureWithCb { ticks, cb: |m| m }.to_action()
+                }
+                TestActions::FailureWithCb { ticks, cb } => {
+                    let mut mock = MockAction::new();
+                    mock.expect_tick()
+                        .times(ticks)
                         .returning(|_dt, _shared| Status::Failure);
                     mock.expect_state().returning(|| State::NoChild);
+                    mock = cb(mock);
+                    Box::new(mock)
                 }
-                TestActions::Run(times, status) => {
+                TestActions::Run { times, output } => TestActions::RunWithCb {
+                    times,
+                    output,
+                    cb: |m| m,
+                }
+                .to_action(),
+                TestActions::RunWithCb { times, output, cb } => {
+                    let mut mock = MockAction::new();
                     mock.expect_tick()
                         .times(times)
                         .returning(|_dt, _shared| Status::Running);
-                    mock.expect_tick().return_once(move |_dt, _shared| status);
+                    mock.expect_tick().return_once(move |_dt, _shared| output);
                     mock.expect_state().returning(|| State::NoChild);
+                    mock = cb(mock);
+                    Box::new(mock)
                 }
                 TestActions::Simulate(cb) => {
+                    let mut mock = MockAction::new();
                     mock = cb(mock);
+                    Box::new(mock)
                 }
             }
-            Box::new(mock)
         }
     }
 }
