279100422aaa524dc6e46109dc1b9849fa8d6ef5
Niket Naidu <coder137@gmail.com>
2026-03-16 01:30:33 -0700

    Updated sequence implementation

--- behaviortree_future/src/behavior_nodes/async_sequence.rs
+++ behaviortree_future/src/behavior_nodes/async_sequence.rs
@@ -68,15 +68,13 @@ where
 }
 
 pub struct AsyncSequence<A> {
-    children: Vec<AsyncBehaviorState<A>>,
-    current_index: usize,
+    inner: AsyncSequenceOrSelect<A>,
 }
 
 impl<A> AsyncSequence<A> {
     pub fn new(children: Vec<AsyncBehaviorState<A>>) -> Self {
         Self {
-            children,
-            current_index: 0,
+            inner: AsyncSequenceOrSelect::new(children, true),
         }
     }
 
@@ -85,10 +83,7 @@ impl<A> AsyncSequence<A> {
         R: BehaviorTreeAsyncRunner<A> + 'static,
         A: Clone + 'static,
     {
-        self.current_index = 0;
-        self.children.iter_mut().for_each(|c| {
-            c.reset(runner.clone(), delta.clone());
-        });
+        self.inner.reset(runner, delta);
     }
 }
 
@@ -103,27 +98,7 @@ where
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Self::Output> {
         let bt = self.as_mut().get_mut();
-
-        let current_child = &mut bt.children[bt.current_index];
-        let child_status = std::pin::pin!(current_child).poll(cx);
-        match child_status {
-            std::task::Poll::Ready(result) => {
-                match result {
-                    true => {
-                        bt.current_index += 1;
-                        if bt.children.get(bt.current_index).is_none() {
-                            std::task::Poll::Ready(true)
-                        } else {
-                            // Tick again to poll the next child
-                            cx.waker().wake_by_ref();
-                            std::task::Poll::Pending
-                        }
-                    }
-                    false => std::task::Poll::Ready(false),
-                }
-            }
-            std::task::Poll::Pending => std::task::Poll::Pending,
-        }
+        std::pin::pin!(&mut bt.inner).poll(cx)
     }
 }
 
